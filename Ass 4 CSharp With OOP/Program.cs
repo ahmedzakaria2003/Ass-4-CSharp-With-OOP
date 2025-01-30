da1dd406b60873dc816d2b6ed12ae4d28c5f6b9f
ahmedzakaria2003 <you@example.com>
2025-01-30 20:48:18 +0200

    Add project files.

---
+++ Ass 4 CSharp With OOP/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Ass_4_CSharp_With_OOP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
